649936f9d1802220870bcb880d350a65261be83b
IpBlDiT <dimokhot@hotmail.com>
2017-09-21 17:50:06 -0400

    Cleanup

--- Queries/PvIsAAA.sql
+++
@@ -1,9 +0,0 @@
-SELECT        
-SM.CUSIP, SM.Name, 
-SM.StructuredProductIndicator, 
-SM.IsStructuredProduct, 
-IIF(A3.CUSIP IS NULL, 0, 1) IsAAA
-FROM            
-pershing.AAA AS A3 RIGHT OUTER JOIN
-pershing.[SecurityMaster:ISCA] AS SM 
-ON A3.CUSIP = SM.CUSIP
